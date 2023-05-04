4c75e17c7a97ea6796f1a0e2d2bead2a1d74763d
VictorCiechovicz <victor09avila@hotmail.com>
2023-05-04 09:01:48 -0300

    feat: create search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,17 +1,68 @@
-
 import Header from './components/Header'
 import SideBar from './components/SideBar'
 import RestaurantCar from './components/RestaurantCar'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
-export default function Search() {
+const fetchRestarantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true
+  }
+
+  if (!city) return prisma.restaurant.findMany({ select })
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase()
+        }
+      }
+    },
+    select
+  })
+}
+
+const fetchLocation = async () => {
+  return prisma.location.findMany()
+}
+const fetchCuisine = async () => {
+  return prisma.cuisine.findMany()
+}
+
+export default async function Search({
+  searchParams
+}: {
+  searchparams: { city: string }
+}) {
+  const restaurant = await fetchRestarantsByCity(searchParams.city)
+  const location = await fetchLocation()
+  const cuisine = await fetchCuisine()
   return (
     <>
       <Header />
-      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SideBar />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start text-black">
+        <SideBar location={location} cuisine={cuisine} />
         <div className="w-5/6">
-          <RestaurantCar />
+          {restaurant.length ? (
+            <>
+              {restaurant.map(rest => (
+                <RestaurantCar
+                  restaurant={rest}
+                  key={rest.id}
+                />
+              ))}
+            </>
+          ) : (
+            <p>Sorry, we found no restaurants in this area.....</p>
+          )}
         </div>
       </div>
     </>
