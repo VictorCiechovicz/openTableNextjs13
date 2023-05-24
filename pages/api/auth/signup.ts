d416f3343aab57e4ce6df66c9545ffa000ec7e95
VictorCiechovicz <victor.ciechovicz@merx.tech>
2023-05-24 16:38:33 -0300

    feat: creat api sgnin with JWT

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,24 +1,33 @@
-import { NextApiResponse,NextApiRequest } from "next";
+import { NextApiResponse, NextApiRequest } from "next";
 import validator from "validator";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import * as jose from 'jose';
 
 
-const prisma= new PrismaClient()
+const prisma = new PrismaClient()
 
 
-export default async function handler( req: NextApiRequest,
+export default async function handler(req: NextApiRequest,
   res: NextApiResponse) {
-  
+
   if (req.method === "POST") {
-    const {firstName,email,phone,city,password} = req.body;
-    const errors: string[] = []; 
+    const { firstName, lastName, email, phone, city, password } = req.body;
+    const errors: string[] = [];
     const validationSchema = [
       {
         valid: validator.isLength(firstName, {
           min: 1,
-          max:20
+          max: 20
         }),
         errorMessage: "First name is Invalid"
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20
+        }),
+        errorMessage: "Last name is Invalid"
       },
       {
         valid: validator.isEmail(email),
@@ -29,22 +38,22 @@ export default async function handler( req: NextApiRequest,
         errorMessage: "Phone number is Invalid"
       },
       {
-        valid: validator.isLength(city,{min:1}),
+        valid: validator.isLength(city, { min: 1 }),
         errorMessage: "City is Invalid"
       },
       {
         valid: validator.isStrongPassword(password),
         errorMessage: "Password is not strong"
-  }
-]
+      }
+    ]
 
     validationSchema.forEach
       ((check) => {
         if (!check.valid) {
           errors.push(check.errorMessage);
-      }
+        }
       })
-    
+
     if (errors.length) {
       return res.status(400).json({ errorMessage: errors[0] });
     }
@@ -55,17 +64,41 @@ export default async function handler( req: NextApiRequest,
         email,
       }
     });
-    
+
     if (userWithEmail) {
-      return res.status(400).json({ errorMessage:"E-mail is associated with another account" });
+      return res.status(400).json({ errorMessage: "E-mail is associated with another account" });
     };
 
-    res.status(200).json(
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        phone,
+        email
+      }
+
+    })
+
+    const alg = "HS256"
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+
+    const token = await new jose.SignJWT({
+      email: user.email
+    }).setProtectedHeader({ alg }).setExpirationTime("24h").sign(secret)
+
+   return res.status(200).json(
       {
-        hello: 'body'
+        hello: token
       });
-    
+
   }
-  
-  
+
+  return res.status(404).json('Unknow endpoint')
+
 };
